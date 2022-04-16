2b81de621f29f18b04a571cb25d92bbf114e7162
Mehran <info.mehranazizi@yahoo.com>
2022-04-16 02:54:14 -0700

    Debug and run build

--- chat-app/src/components/Chats.js
+++ chat-app/src/components/Chats.js
@@ -15,56 +15,57 @@ import { AuthContext } from "../context/AuthContextProvider";
 const Chats = () => {
   const [loading, setLoading] = useState(true);
   const user = useContext(AuthContext);
-  console.log(user)
-
   const history = useHistory();
+
   const logOutHandler = async () => {
     await auth.signOut();
     history.push("/");
   };
 
+
   useEffect(() => {
-      if(!user) {
-          history.push("/")
-      }
+    if (!user) {
+      history.push("/");
+      return;
+    }
 
-      axios.get("https://api.chatengine.io/users/me" , {
-          Headers :{
-              "project-id" : "f5cd3659-10ae-48b3-9b39-cd98a6f7aefe",
-              "user-name" : user.email,
-              "user-secret" : user.uid
-          }
+    axios.get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "f5cd3659-10ae-48b3-9b39-cd98a6f7aefe",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
       })
-      .then (() => {
-          setLoading(false)
+      .then(() => {
+        setLoading(false);
+        console.log(user)
       })
-      .catch (() => {
-          let formdata = new FormData();
-          formdata.append("email" , user.email);
-          formdata.append("username" , user.email);
-          formdata.append("secret" , user.uid);
-          getFile(user.photoURL)
-            .then(avatar => {
-                formdata.append("avatar" , avatar , avatar.name);
-                axios.post("https://api.chatengine.io/users/" , formdata , {
-                    headers : {
-                        "private-key" : "7d254c98-5bad-4f3d-8056-14468a4e5d82"
-                    }
-                })
-                .then(() => setLoading(false))
-                .catch((error => console.log(error)))
+      .catch((error) => {
+        console.log(error);
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+        getFile(user.photoURL).then((avatar) => {
+          formdata.append("avatar", avatar, avatar.name);
+          axios.post("https://api.chatengine.io/users/", formdata, {
+              headers: {
+                "private-key": "7d254c98-5bad-4f3d-8056-14468a4e5d82",
+              },
             })
-      })
-
-  } , [user, history])
+            .then(() => setLoading(false))
+            .catch((error) => console.log(error));
+        });
+      });
+  }, [user, history]);
 
   const getFile = async (url) => {
-      const response = await fetch(url);
-      const data = await response.blob();
-      return new File([data] , "userPhoto.jpg" , {type : "jpeg"})
-  }
+    const response = await fetch(url);
+    const data = await response.blob();
+    return new File([data], "userPhoto.jpg", { type: "jpeg" });
+  };
 
-  if(!user || loading) return "Loading..."
+  if (!user || loading) return "Loading...";
 
   return (
     <div>
@@ -72,8 +73,8 @@ const Chats = () => {
       <ChatEngine
         height="calc(100vh - 60px)"
         projectID="f5cd3659-10ae-48b3-9b39-cd98a6f7aefe"
-        userName= {user.email}
-        userSecret= {user.uid}
+        userName={user.email}
+        userSecret={user.uid}
       />
     </div>
   );
